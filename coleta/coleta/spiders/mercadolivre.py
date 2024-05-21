ab4e1dbca4262877c20d1bc9ad2b56c6e60117e3
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-05-21 20:16:56 -0300

    first commit

---
+++ coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+
+    def parse(self, response):
+        pass
